1dc158a996d848521bf381e2422e5bae6882ddd3
Ben Pilgrim <ben@pilgrim.me.uk>
2016-12-06 19:13:03 +0000

    Database migrations + fixes

--- config/app.php
+++ config/app.php
@@ -172,11 +172,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
+        Terra\Bifrost\Providers\AppServiceProvider::class,
+        Terra\Bifrost\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        Terra\Bifrost\Providers\EventServiceProvider::class,
+        Terra\Bifrost\Providers\RouteServiceProvider::class,
 
     ],
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+		Eloquent::unguard();
+		$this->call('OAuthClientsSeeder');
+		$this->call('OAuthUsersSeeder');
     }
+}
+
+class OAuthClientsSeeder extends Seeder
+{
+	public function run()
+	{
+		DB::table('oauth_clients')->delete();
+		
+		DB::table('oauth_clients')->insert(array(
+			'client_id' => "demoapp",
+			'client_secret' => "demopass",
+			'redirect_uri' => "https://www.getpostman.com/oauth2/callback",
+		));
+	}
+}
+class OAuthUsersSeeder extends Seeder
+{
+	public function run()
+	{
+		DB::table('oauth_users')->delete();
+		
+		DB::table('oauth_users')->insert(array(
+			'username' => "user",
+			'password' => sha1("password"),
+			'first_name' => "First",
+			'last_name' => "Name",
+		));
+	}
 }
